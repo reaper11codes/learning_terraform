e6c2649425c34f08e16ca412feb51069cd9408b0
Akhil Ahuja <akhil1ahuja@gmail.com>
2025-05-06 19:59:06 +0530

    Adding an action to the listeners.

--- main.tf
+++ main.tf
@@ -58,7 +58,9 @@ module "alb" {
     ex-http = {
       port     = 80
       protocol = "HTTP"
-      target_group_index = 0
+      forward = {
+        target_group_index = 0
+      }
     }
   }
 
