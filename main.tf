74ceea74e564a0e45ffa9d719c542b1cdf4a8bfc
Akhil Ahuja <akhil1ahuja@gmail.com>
2025-05-06 20:16:19 +0530

    Pinning older version of lib

--- main.tf
+++ main.tf
@@ -48,20 +48,20 @@ resource "aws_instance" "blog" {
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http = {
-      port     = 80
-      protocol = "HTTP"
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
       target_group_index = 0
-      type     = "forward"
     }
-  }
+  ]
 
   target_groups = {
     ex-instance = {
